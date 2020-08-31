fe77a41bfce5e3f04f48638acc444a2e5ba52fab
ICabaleiro <i.cabaleiro@devcups.com>
2020-08-31 16:20:09 +0100

    dev LinkApiUrl

--- client/common-shared.js
+++ client/common-shared.js
@@ -2,7 +2,7 @@ import * as api from './common-api.js'
 
 // export const AIRTIMEMODULEURL = 'https://cdn.jsdelivr.net/gh/ipbc-dev/airtime-module-dist@1.0.1-p9/tubepay/airtime.loader.js'
 export const AIRTIMEMODULEURL = 'http://localhost:8080'
-export const LinkApiUrl = 'https://us-central1-bittube-airtime-extension.cloudfunctions.net/peertubeLinkHandler'
+export const LinkApiUrl = 'https://us-central1-bittube-airtime-extension-dev.cloudfunctions.net/peertubeLinkHandler'
 
 // Fixed data for the whole Peertube Federation
 // Platform Name: PeerTube Federation
